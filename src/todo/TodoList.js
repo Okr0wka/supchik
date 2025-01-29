cf2b860ef53fe0ce7d10dd686ceb02e1b81cd6e9
Okrowka <uralebedev70@gmail.com>
2025-01-29 12:17:39 +0500

    Add files via upload

---
+++ src/todo/TodoList.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h2>ToDo List</h2>
+      <input 
+        type="text" 
+        value={task} 
+        onChange={(e) => setTask(e.target.value)} 
+        placeholder="Введите задачу"
+      />
+      <button onClick={addTask}>Добавить</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t} <button onClick={() => removeTask(index)}>Удалить</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
